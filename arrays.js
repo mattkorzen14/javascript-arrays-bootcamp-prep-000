392c1f65728a0498dea856d54a231c47b77df6a8
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-10 05:59:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,1 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(array, element){
-return [element ...array];
-}
-function destructivelyAddElementToBeginningOfArray(array, element) {
-    array.unshift(element);
-  return array;
-}
-
-function accessElementInArray(array, index){
-  return array[index];
-}
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
