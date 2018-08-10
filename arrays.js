5575fded6b8916b1842a96c6213d8d7b8fdefeb3
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-10 06:04:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element)
 }
+}
 function addElementToEndOfArray {
   return [...array, element]
 }
